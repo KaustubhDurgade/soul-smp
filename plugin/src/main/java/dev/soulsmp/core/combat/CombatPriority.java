e425242d604c933699cc8a5d80adb020e3eea080
KaustubhDurgade <KaustubhDurgade@users.noreply.github.com>
2025-10-02 16:11:29 -0700

    fuck me

--- plugin/src/main/java/dev/soulsmp/core/combat/CombatPriority.java
+++
@@ -1,9 +0,0 @@
-package dev.soulsmp.core.combat;
-
-public enum CombatPriority {
-    HIGHEST,
-    HIGH,
-    NORMAL,
-    LOW,
-    LOWEST
-}
--- plugin/src/main/java/dev/soulsmp/core/effect/EffectStackingPolicy.java
+++
@@ -1,8 +0,0 @@
-package dev.soulsmp.core.effect;
-
-public enum EffectStackingPolicy {
-    NONE,
-    REFRESH,
-    STACK_DURATION,
-    STACK_AMPLIFIER
-}
